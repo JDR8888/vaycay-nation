167e90e09269e6715f9266240116f7b0edc7f9da
jake rudlong <jrudlong@gmail.com>
2023-04-28 18:05:47 -0500

    imports bootstrap and sets up basic nav component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import './App.css';
+// import './App.css';
 // import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import SearchResults from './pages/SearchResults';
 import Signup from './pages/Signup';
+import Navbar from './components/Nav';
 
 
 const client = new ApolloClient({
@@ -32,6 +34,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/profile' element={ <Profile /> } />

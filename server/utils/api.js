ff7321d5dddc86c7f39aa344ff41f0ec7ab7e91c
jake rudlong <jrudlong@gmail.com>
2023-04-28 17:27:52 -0500

    sets up fetchParks async function in backend

--- server/utils/api.js
+++ server/utils/api.js
@@ -4,3 +4,14 @@ const axios = require('axios');
 const apiKey = process.env.API_KEY;
 
 //  set up function getParkByState (take param for state, and reference apiKey )
+async function fetchParks(state, apiKey) {
+  try {
+    const response = await axios.get(`https://developer.nps.gov/api/v1/parks?stateCode=${state}&api_key=${apiKey}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// example of calling function:
+// fetchParks('MN', apiKey);

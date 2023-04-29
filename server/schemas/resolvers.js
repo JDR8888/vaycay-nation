a335cf5011c6e434a3dbaac66211456aec5b8c65
Jared A <jroberts1931@gmail.com>
2023-04-29 19:19:44 -0400

    merge

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,38 @@
+// const resolvers = {
+//   Query: {
+//     users: async () => {
+//       return [
+//         {
+//           _id: 'asdioufo9a8rtjaod8iosmf',
+//           username: 'Mork',
+//           email: 'mork@nanoo.org',
+//           password: 'youllneverguess',
+//         },
+//       ];
+//     },
+//   },
+// };
+
+// module.exports = resolvers;
+require('dotenv').config();
+// const ObjectId = require('mongoose').Types.ObjectId;
+const { fetchParks, fetchPark } = require('../utils/api.js');
+// const { AuthenticationError } = require('apollo-server-express');
+// const { User } = require('../models');
+// const { signToken } = require('../utils/auth');
+
 const resolvers = {
   Query: {
-    users: async () => {
-      return [
-        {
-          _id: 'asdioufo9a8rtjaod8iosmf',
-          username: 'Mork',
-          email: 'mork@nanoo.org',
-          password: 'youllneverguess',
-        },
-      ];
+    // Returns all parks
+    parks: async (_, { park }) => {
+      const data = await fetchParks(park);
+      return data.results;
+    },
+
+    // Returns a park
+    park: async (_, { parkId }) => {
+      const data = await fetchPark(parkId);
+      return data;
     },
   },
 };

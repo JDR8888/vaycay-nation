df09dd6f117520f8d2de901f5b2a93a8ca84bf71
jake rudlong <jrudlong@gmail.com>
2023-04-29 17:26:04 -0500

    moves .env into server folder and removes quotes from env key

--- server/utils/api.js
+++ server/utils/api.js
@@ -1,7 +1,7 @@
 // bring in axios for making api calls
 const axios = require('axios');
 // grab our api key from dotenv
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.REACT_APP_API_KEY;
 
 
 //  set up function getParkByState (take param for state, and reference apiKey )

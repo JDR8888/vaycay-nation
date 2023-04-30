3e5281c03d908337809daa9dec99816cbc18ac0f
jake rudlong <jrudlong@gmail.com>
2023-04-29 23:39:56 -0500

    adds content to homepage, adds fonts, incorporates signin modal to homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 // import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
@@ -10,21 +10,10 @@ import SearchResults from './pages/SearchResults';
 import Signup from './pages/Signup';
 import Navbar from './components/Nav';
 
-
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
-
-
-// axios.get('https://developer.nps.gov/api/v1/parks?stateCode=AK&api_key=TRwtUPRfynEen7iIp3GN9eZAgjkJ0MVIQOC7dmly')
-//   .then(response => {
-//     console.log(response.data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-
 
 function App() {
   const [count, setCount] = useState(0);
@@ -37,7 +26,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/profile' element={ <Profile /> } />
           <Route path='/searchResults' element={ <SearchResults /> } />
-
+          <Route path='/Signup' element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+// import Auth from '../utils/auth.js'
 
 
-export default function Home () {
 
-    return (
-        <h1> this is the home page </h1>
-    )
+function Signin() {
+  return (
+    <Form style={{fontFamily: 'Oxygen'}}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button
+      style={{backgroundColor: '#1B5448', marginRight: '1em' }}
+       type="submit">
+        Sign in!
+      </Button>
+      <Button variant="secondary" type="submit">
+        Nevermind
+      </Button>
+      <Link to='/Signup'>
+        <p>Don't have an account yet? Sign up!</p>
+      </Link>
+    </Form>
+  );
 }
 
+export default Signin;

7ad3723e249cef05217a3f8d28625263be23e00e
jake rudlong <jrudlong@gmail.com>
2023-04-30 15:22:17 -0500

    adds parkCard component, gets parkByState query working and results loading

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,20 @@
 import { gql } from "@apollo/client";
 
-// export const GET_PARKS_BY_STATE = gql`
-// query GetParksByState($state: String) {
-//     getParksByState(state: $state) {
-//       fullName
-//       weatherInfo
-//       id
-//       description
-//       images {
-//         url
-//         altText
-//       }
-//       latitude
-//       longitude
-//       operatingHours {
-//         description
-//       }
-//     }
-//   }`;
+export const GET_PARKS_BY_STATE = gql`
+query GetParksByState($state: String) {
+    getParksByState(state: $state) {
+      fullName
+      weatherInfo
+      id
+      description
+      images {
+        url
+        altText
+      }
+      latitude
+      longitude
+      operatingHours {
+        description
+      }
+    }
+  }`;

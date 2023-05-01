9c8ff9eca1e777f49aa4261f6a30242c393dcd4d
Jared A <jroberts1931@gmail.com>
2023-05-01 16:10:53 -0400

    used react bootstrap instead of just bootstrap components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 import logo from '../assets/images/vaycaylogo.png';
 
-export default function NavbarFooter() {
+function ColorSchemesExample() {
   return (
-    <div style={{ paddingTop: '10em' }}>
-      <Navbar bg="dark" fixed="bottom">
-        <Container className="justify-content-center">
-          <Navbar.Brand href="#">
-            <img
-              alt=""
-              src={logo}
-              width="30"
-              height="24"
-              className="d-inline-block align-top"
-            />{' '}
-            <span className="text-light">VayCay Nation</span>
+    <>
+      <Navbar bg="dark" variant="dark" fixed="bottom">
+        <Container className="d-flex justify-content-center">
+          <img src={logo} width="40" height="40" />
+          <Navbar.Brand href="#home" className="mx-2">
+            VayCay Nation
           </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">
+              <FaInstagram size={20} />{' '}
+            </Nav.Link>
+            <Nav.Link href="#features">
+              <FaTwitter size={20} />
+            </Nav.Link>
+            <Nav.Link href="#pricing">
+              {' '}
+              <FaFacebook size={20} />
+            </Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
-    </div>
+    </>
   );
 }
+
+export default ColorSchemesExample;

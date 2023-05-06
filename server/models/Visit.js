64236c4dd20c93d6f754d015b66c3201b1a6a594
jake rudlong <jrudlong@gmail.com>
2023-05-06 15:09:10 -0500

    adds mongodb to package.json

--- server/models/Visit.js
+++ server/models/Visit.js
@@ -1,3 +1,25 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const visitSchema = new Schema({
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => timestamp.toLocaleDateString().concat(' ', timestamp.toLocaleTimeString()),
+    },
+    park: {
+        type: String,
+        required: true,
+    },
+    review: {
+        type: String
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+
+
+})
